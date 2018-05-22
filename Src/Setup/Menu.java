162515c94ac7560b7a83ebb55ba9ba8e17c36a63
Unknown <sander.quist@hotmail.nl>
2018-05-22 10:25:58 +0200

    Changed Menu classes

--- Src/Setup/Menu.java
+++ Src/Setup/Menu.java
@@ -1,21 +1,28 @@
-package Setup;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.border.Border;
+import java.awt.*;
 
-public class Menu extends JFrame implements ActionListener
+public class Menu extends JFrame
 {
+    private String tijd;
+    private JButton steen;
+    private JButton papier;
+    private JButton schaar;
+    private JLabel timer;
+
     public Menu()
     {
-        // play button
-        // connect
-        // settings
-        //
+        JPanel content = new JPanel(new BorderLayout());
+        JPanel center = new JPanel(new GridLayout(1,3));
+        content.add(center, BorderLayout.CENTER);
+        setupComponents();
+
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public void setupComponents(){
+        steen = new JButton("Steen");
+        papier = new JButton("Papier");
+        schaar = new JButton("Schaar");
+        timer = new JLabel("Tijd:" + tijd);
     }
 }
