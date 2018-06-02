fdd297f89a7480f63cdd4296452819d9eb3cb77f
Unknown <sander.quist@hotmail.nl>
2018-06-02 14:41:21 +0200

    Changed menu layout

--- Src/Setup/Menu.java
+++ Src/Setup/Menu.java
@@ -8,6 +8,7 @@ import Game.Steen;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
 import java.io.IOException;
 
 public class Menu extends JFrame
@@ -26,13 +27,15 @@ public class Menu extends JFrame
     private Object steenos = new Steen();
     private Object choice = null;
     private Object object = new Object("Kip");
+    private JLabel whiteSpace1;
+    private JLabel whiteSpace2;
 
 
     public Menu() throws IOException {
         menu = new JFrame();
         content = new JPanel(new BorderLayout());
         menu.add(content);
-        center = new JPanel(new GridLayout(1,3));
+        center = new JPanel(new GridBagLayout());
         content.add(center, BorderLayout.CENTER);
         upperBar = new JPanel();
         content.add(upperBar,BorderLayout.NORTH);
@@ -42,6 +45,13 @@ public class Menu extends JFrame
         menu.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         menu.setVisible(true);
+    }
+
+    public void paintComponent(Graphics2D g){
+        super.paintComponents(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.drawLine(1000,1000,1000,1000);
 
     }
 
@@ -50,6 +60,8 @@ public class Menu extends JFrame
         papier = new JButton(papieros.getPlaatje());
         schaar = new JButton(schaaros.getPlaatje());
         timer = new JLabel("Tijd:" + tijd);
+        whiteSpace1 = new JLabel("");
+        whiteSpace2 = new JLabel("");
 
         timer.setFont(new Font("TimesRoman", 1, 20));
 
@@ -69,11 +81,20 @@ public class Menu extends JFrame
             System.out.println(choice.getNaam());
         });
 
+        steen.setPreferredSize(new Dimension(135,181));
+        papier.setPreferredSize(new Dimension(136,181));
+        schaar.setPreferredSize(new Dimension(230,181));
+        whiteSpace1.setPreferredSize(new Dimension(100,100));
+        whiteSpace2.setPreferredSize(new Dimension(100,100));
+
         center.add(steen);
-        center.add(papier);
+        center.add(whiteSpace1);
         center.add(schaar);
+        center.add(whiteSpace2);
+        center.add(papier);
         upperBar.add(timer);
+    }
     }
 
 
-}
+
